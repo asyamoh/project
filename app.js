1b58bbfb183715a906c22a93b93a04b0fc3521eb
asyamoh <asia_nazif@yahoo.com>
2021-05-23 18:38:53 +0300

    updated

---
+++ app.js
@@ -0,0 +1,20 @@
+alert('welcome')
+
+let userAge = prompt ('how old are you?')
+alert (' if you are < 18 you probaply having an exams and you should be studying right NOW !')
+console.log('how old are you?'+ ' ' + userAge);
+
+let userChoice = prompt('Whats your favourite show or movie emily in paris or HTGAWM?')
+console.log('Whats your favourite show or movie emily in paris or HTGAWM?'+' ' +userChoice );
+
+
+
+
+  alert ('thank you for passing by :)')
+   let user = prompt ('when was the last time you watch a horror movie?')
+   console.log('when was the last time you watch a horror movie?'+' ' + user);
+   alert ('aww it has been LONG time')
+
+   let userAnser = prompt ('would you like a suggestions?')
+   console.log('would you like a suggestions?'+' ' +userAnswer);
+   alert ('thanks for your trust!')
